4343c913b5ad8882625efdc7e19113d498870c8e
Marte Jørgensen <martejorgensen@pc167-74.hiof.no>
2015-05-20 16:20:15 +0200

    Added javascript for the pip_add_likes button. So now it works.

---
+++ kvittr/theme/static/theme/js/kvittr.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $(".pip_add_likes").click(function(event) {
+        event.preventDefault();
+        var pip_id = $(this).data("pip_id");
+        $.ajax({
+            url: '/pips_messages/pip_add_likes/' + pip_id
+        })
+        .done(function(data){
+            console.log("done");
+            var likes_updated = data['likes_updated'];
+            console.log(data);
+            var pip_add_likes = "#id-points-for-pip-" + pip_id;
+            $(pip_add_likes).html('likes_updated');
+           
+        });
+    });
+});
