76671c9eab165e27d953772ae0bcfc91ff1467c2
Erika <erika.sandbaek@hotmail.com>
2015-05-20 15:42:07 +0200

    Created model for pips_messages in models.py

--- kvittr/pips_messages/models.py
+++ kvittr/pips_messages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Pip(models.Model):
+    pip = models.TextField()
+    created_datetime = models.DateTimeField()
+    created_by = models.ForeignKey(User, related_name="created_pip")
+    likes = models.PositiveIntegerField(default=0)  
+
+    def __unicode__(self):
+        return u'%s' % self.pip
+
+    class Meta:
+        ordering = ['-id']
