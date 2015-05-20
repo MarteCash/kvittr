3b44a2e54c8010b1b7fe9cbdae95141682bddb35
Erika <erika.sandbaek@hotmail.com>
2015-05-20 15:20:21 +0200

    Created views for 'birds'with requests for register, login and logout

--- kvittr/birds/views.py
+++ kvittr/birds/views.py
@@ -1,3 +1,34 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+def bird_login(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('frontpage')
+        else:
+            context['login_failed'] = True
+    return render(request, 'birds/login.html', context)
+
+def bird_logout(request):
+    logout(request)
+    return redirect('frontpage')
+
+def bird_register(request):
+    context = {}
+    if request.method == "POST":
+        user = User()
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.set_password(request.POST.get('password'))
+        user.save()
+        context['user_saved_successfully'] = True
+    return render(request, 'birds/register.html', context)
