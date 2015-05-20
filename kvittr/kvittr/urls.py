90bef07510145f566b5aa2076e9c256109ebde78
Erika <erika.sandbaek@hotmail.com>
2015-05-20 15:39:05 +0200

    Included url for pips_messages in urls.py in kvittr-project

--- kvittr/kvittr/urls.py
+++ kvittr/kvittr/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('theme.urls')),
     url(r'^birds/', include('birds.urls')),
+    url(r'^pips_messages/', include('pips_messages.urls')),
+]
 ]
