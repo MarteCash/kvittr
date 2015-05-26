c7b353e201f77fa211c79a90ce4777cda6c9602e
Erika <erika.sandbaek@hotmail.com>
2015-05-26 12:08:54 +0200

    Added url for 'profile' in birds/urls.py

--- kvittr/birds/urls.py
+++ kvittr/birds/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^login$', views.bird_login, name='user_login'),
     url(r'^logout$', views.bird_logout, name='user_logout'),
     url(r'^register$', views.bird_register, name='user_register'),
+    url(r'^profile$', views.bird_profile, name='user_profile'),
 )
